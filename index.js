a75f84a48d64030873aa874e962799ac2e6a9b8e
Chadrac-WAKOMYA <chadracxx@gmail.com>
2026-02-10 11:58:36 +0100

    index page updated

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-        import { useState } from 'react';
+
+import LikeButton from './app/like-button';
         
-        function Header({title}){
-          return <h1>{title ? title : 'Default Title'}</h1>;
-        }
+function Header({title}){
+  return <h1>{title ? title : 'Default Title'}</h1>;
+}
 
-        export default function HomePage(){
-          
-          const names = ['NextJs', 'NestJs', 'ReactJs'];
+export default function HomePage(){
+  
+  const names = ['NextJs', 'NestJs', 'ReactJs'];
 
-          const [likes, setLikes] = React.useState(0);
-          
-          function handleClick(){
-            setLikes(likes + 1);
-          }
-          function handleClick2(){
-            setLikes(likes -1);
-          }
-          return(
-            <div>
-              <Header title="React is the best"/>
-              <Header title="Develop. Preview. Ship."/>
-              <ul>
-                {names.map((name)=>(
-                  <li key={name}>{name}</li>
-                ))}
-              </ul>
-              <button onClick={handleClick}>Like{likes}</button>
-              <button onClick={handleClick2}>UnLike{likes}</button>
-            </div>
-          );
-        }    
+  return(
+    <div>
+      <Header title="React is the best"/>
+      <Header title="Develop. Preview. Ship."/>
+      <ul>
+        {names.map((name)=>(
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <LikeButton />
+    </div>
+  );
+}    
    
